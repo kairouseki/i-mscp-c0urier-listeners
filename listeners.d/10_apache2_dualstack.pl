750c75f705cc370c582b86dacc163f5f12b5ec03
Laurent Declercq <l.declercq@nuxwin.com>
2015-01-01 10:21:15 +0100

    Added Listener::Apache2::DualStack listener file (dev in progress...)

--- listeners.d/10_apache2_dualstack.pl
+++ listeners.d/10_apache2_dualstack.pl
@@ -1,126 +1,92 @@
-=head1 NAME
-
- Listener::Apache2::DualStack
-
-=cut
-
+# i-MSCP Listener::Apache2::DualStack listener file
+# Copyright (C) 2015 Laurent Declercq <l.declercq@nuxwin.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 
 package Listener::Apache2::DualStack;
 
-use iMSCP::EventsManager;
+use iMSCP::EventManager;
 use List::MoreUtils qw(uniq);
 
-=head1 DESCRIPTION
-
- Listener file which allow to add additional IPs (DualStack) in Apache2 vhost files managed by i-MSCP
-
-=head1 CONFIGURATION PARAMETERS
-
-=over 4
-
 my $httpPort = 80;
-my $httpsPort = 443:
+my $httpsPort = 443;
 
 # Parameter allowing to add one or many IPs to the Apache2 vhost file of a specific domain
 # IPv6 addresses must be surrounded by square-brackets (eg )[2001:db8:0:85a3:0:0:ac1f:8001]
 # Please replace the values below by true values
-my %perDomainAdditionalIPs = (
-	'<domain1.tld>' => [ '<ip1>', '<ip2>' ],
-	'<domain2.tld>' => [ '<ip1>', '<ip2>' ]
-);
+my %perDomainAdditionalIPs = ();
 
 # Parameter which allow to add one or many IPs to all apache2 vhosts files
 # IPv6 addresses must be surrounded by square-brackets (eg )[2001:db8:0:85a3:0:0:ac1f:8001]
 # Please replace the values below by true values
-my @additionalIps = ('<ip1>', '<ip2>' );
+my @additionalIps = ('192.168.5.118', '192.168.5.119' );
 
-# Please, don't edit anything below this line
+#
+## Please, don't edit anything below this line
+#
 
-=back
-
-=head1 EVENT LISTENERS
-
-=over 4
-
-=item addNameVirtualHost
-
- Listener responsible to add NameVirtualHost directives
-
-=cut
-
-sub addNameVirtualHost
+# Listener responsible to make the Httpd server implementation aware of additional IPs
+sub addIPList
 {
-	my ($cfgTpl, $data) = @_;
+	my $data = $_[1];
 
 	my @ipsList = ();
 
-	# Build list of named Directives
 	for my $domain(keys %perDomainAdditionalIPs) {
-		@ipsList = (@ipsList, @{$perDomainAdditionalIPs{$domain}} );
+		@ipsList = ( @ipsList, @{$perDomainAdditionalIPs{$domain}} );
 	}
 
-	@ipsList = (@ipsList, @additionalIps);
+	@ipsList = s/\[|\]//g for @ipsList, @additionalIps;
 
 	#@{$data->{'SSL_IPS'}} = uniq(@{$data->{'SSL_IPS'}}, @ipsList);
-	@{$data->{'IPS'}} = uniq(@{$data->{'IPS'}}, @ipsList);
+	@{$data->{'IPS'}} = uniq( @{$data->{'IPS'}}, @ipsList );
 
 	0;
 }
 
-=item addIps(\$cfgTpl, $tplName, \%data)
-
- Listener responsible to add additional IPs in Apache2 vhost files
-
- Param string \$cfgTpl Apache2 vhost template
- Param string $tplName Apache vhost template name (domain.tpl, domain_ssl.tpl domain_redirect.tpl ...)
- Param hash \%data Data as provided by Httpd server implementation
- Return int 0
-
-=cut
-
-sub addIps
+# Listener responsible to add additional IPs in Apache2 vhost files
+sub addIPs
 {
 	my ($cfgTpl, $tplName, $data) = @_;
 
-	if(exists $data->{'DOMAIN_NAME'}) {
-		if($tplName ~= /^domain_(disabled|redirect)?(_ssl)?\.tpl/) {
-			my $port = ($tplName ~= /ssl/) ? $httpsPort : $httpPort;
+	if(exists $data->{'DOMAIN_NAME'} && $tplName =~ /^domain(?:_(?:disabled|redirect))?(_ssl)?\.tpl$/) {
+		my $port = (defined $1) ? $httpsPort : $httpPort;
+		my $ipList = ();
 
-			# All vhost IPs
-			if(@additionalIps) {
-				my $ipsListStr = join ":$port ", @additionalIps;
-				$$cfgTpl ~= s/<VirtualHost(.*)>/<VirtualHost $1 $ipsListStr>/;
-			}
+		# All vhost IPs
+		if(@additionalIps) {
+			@ipList = split ' ', (join ":$port ", @additionalIps) . ":$port";
+		}
 
-			# Per domain IPs
-			if(%perDomainAdditionalIPs && exists $perDomainAdditionalIPs{$data->{'DOMAIN_NAME'}}) {
-				my $ipsListStr = join ":$port ", @{$perDomainAdditionalIPs{$data->{'DOMAIN_NAME'}}};
-				$$cfgTpl ~= s/<VirtualHost(.*)>/<VirtualHost $1 $ipsListStr>/;
-			}
+		# Per domain IPs
+		if(exists $perDomainAdditionalIPs{$data->{'DOMAIN_NAME'}}) {
+			@ipList = uniq(@ipList, split ' ', (join ":$port ", @{$perDomainAdditionalIPs{$data->{'DOMAIN_NAME'}}}) . ":$port");
+		}
+
+		if(@ipList) {
+			$$cfgTpl =~ s/(<VirtualHost.*?)>/$1 @ipList>/;
 		}
 	}
 
 	0;
 }
 
-=back
-
-=head1 MAIN
-
-=over 4
-
-# Register event listeners on the events manager
-my $eventsManager = iMSCP::EventsManager->getInstance();
-$eventsManager->register('beforeHttpdAddIps', \&addNameVirtualHost);
-$eventsManager->register('afterHttpdBuildConfFile', \&addIps);
-
-=back
-
-=head1 AUTHOR
-
- Laurent Declercq <l.declercq@nuxwin.com>
-
-=cut
+# Register event listeners on the event manager
+my $eventManager = iMSCP::EventManager->getInstance();
+$eventManager->register('beforeHttpdAddIps', \&addIPList);
+$eventManager->register('afterHttpdBuildConfFile', \&addIPs);
 
 1;
 __END__
